e238ca9a2ac8b9c0d3f8db9735c447637219eee9
Mark A Skurat <markskurat136@gmail.com>
2019-10-20 21:09:54 -0400

    string assignment

---
+++ mp_1/week_1/2_vartypes_todo.py
@@ -0,0 +1,12 @@
+"""
+Task- variables and types
+"""
+
+# TODO: make a string and concatenate it with numbers, then print the result
+string1 = "Mark"
+string2 = "1000"
+string1 + string2
+print(string1+ string2)
+# TODO: set a float equal to a variable, then print that float
+
+# TODO: use the function `type(variable-here)` to print examples of the types we have worked
